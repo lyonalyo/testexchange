2c87b83b24f3346f96393f8a11e962fd6c3dfcd9
lyonalyo <e.shterenberg@gmail.com>
2020-06-16 00:31:10 +0300

    Updates

---
+++ precache-manifest.9e755eaec1a91fa7102fd0c5a12b4154.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c4f72cea6c637794ee8a08ea0fa8f9e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "03491aa7cc4590c3c253",
+    "url": "./static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "5904c3dab0f23cd8253b",
+    "url": "./static/css/main.48330483.chunk.css"
+  },
+  {
+    "revision": "03491aa7cc4590c3c253",
+    "url": "./static/js/2.ea86b21f.chunk.js"
+  },
+  {
+    "revision": "b0474469b72f71b5f0ea9f20119d7572",
+    "url": "./static/js/2.ea86b21f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5904c3dab0f23cd8253b",
+    "url": "./static/js/main.386bd235.chunk.js"
+  },
+  {
+    "revision": "e856331ba0e6392f8e4a",
+    "url": "./static/js/runtime-main.0c77eafb.js"
+  },
+  {
+    "revision": "4c51214613dc35465553e71e131a4cb3",
+    "url": "./static/media/logo.4c512146.svg"
+  }
+]);
